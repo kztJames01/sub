fb686201f2e817413e0eda41d25a7e7c93ed70cf
Kaung Zaw Thant <66521022+kztJames01@users.noreply.github.com>
2025-02-12 10:18:57 -0600

    Still modifying

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { getUsers, getUser } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/users", (req, res) => res.send({ title: "GET All User"}));
-userRouter.get("/:id", (req, res) => res.send({ title: "GET User Detail"}));
+userRouter.get("/users", getUsers);
+userRouter.get("/:id", getUser);
 userRouter.post("/", (req, res) => res.send({ title: "Create User"}));
 userRouter.put("/:id", (req, res) => res.send({ title: "Update User"}));
 userRouter.delete("/:id", (req, res) => res.send({ title: "Delete User"}));
