3715e5fa7e3273a97ff9ac0a99e55b794113cfd6
Kaung Zaw Thant <66521022+kztJames01@users.noreply.github.com>
2025-02-12 10:19:49 -0600

    Updated sub model with presave function for missing renewals

--- models/user.models.js
+++ models/user.models.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Password is required'],
         minlength: [6, 'Password must be at least 6 characters long'],
     }
-}, options = { timestamps: true }
+}, { timestamps: true }
 );
 
 const UserModel = mongoose.model("User", userSchema);
