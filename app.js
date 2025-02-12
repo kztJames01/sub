d1f3f6ebc54b5a76a6bf1ccb2c59a24482a1de90
Kaung Zaw Thant <kaung.thant@aiesec.net>
2025-02-12 07:07:18 -0600

    Continue dev

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import express from "express";
+
 import { PORT } from "./config/env.js";
+
+import connectToDatabase from "./database/mongodb.js";
+import subRouter from "./routes/sub.routers.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";  
+
 const app = express();
+app.use("/api/v1/subs", subRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT} https://localhost:${PORT}`);
+app.listen(PORT, async () => {
+  console.log(`Listening on port ${PORT} http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
